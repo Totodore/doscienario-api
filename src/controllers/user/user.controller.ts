4905f0a1b76cae79e9fde3c8e27774f7981aaea5
Elariondakta <prevottheodore@gmail.com>
2021-02-13 19:21:59 +0100

    verifying routes

--- src/controllers/user/user.controller.ts
+++ src/controllers/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, ForbiddenException, Get, Patch, Post, UseGuards } from '@nestjs/common';
+import { BadRequestException, Body, Controller, ForbiddenException, Get, Patch, Post, UseGuards } from '@nestjs/common';
 import { GetUser } from 'src/decorators/user.decorator';
 import { UserGuard } from 'src/guards/user.guard';
 import { User } from 'src/models/user.entity';
@@ -7,7 +7,6 @@ import { JwtService } from 'src/services/jwt.service';
 import { UserAuthDto } from './user-auth.dto';
 import { UserNameDto } from './user-name.dto';
 import { UserPassDto } from './user-pass.dto';
-import { UserRes } from './user.res';
 
 @Controller('user')
 export class UserController {
@@ -19,7 +18,7 @@ export class UserController {
 
   @Post("/auth")
   async auth(@Body() body: UserAuthDto): Promise<string> {
-    const user = (await User.findOne({ where: { name: body.name }, select: ["password", "id"] }));
+    const user = (await User.findOne({ where: { name: body.name.toLowerCase() }, select: ["password", "id"] }));
 
     if (!user || !this._jwt.verifyPassword(user.password, body.password))
       throw new ForbiddenException();
@@ -31,34 +30,37 @@ export class UserController {
   @Post("/register")
   async register(@Body() body: UserAuthDto): Promise<string> {
     const hash = this._jwt.encodePassword(body.password);
-    const user = await User.create({ name: body.name, password: hash }).save();
+    if (await User.exists({ where: { name: body.name.toLowerCase() } }))
+      throw new BadRequestException();
+    const user = await User.create({ name: body.name.toLowerCase(), password: hash }).save();
     const jwt = this._jwt.encode(user.id);
     return jwt;
   }
 
   @Patch("/password")
   @UseGuards(UserGuard)
-  async updatePass(@Body() body: UserPassDto, @GetUser() user: User) {
+  async updatePass(@Body() body: UserPassDto, @GetUser() user: User): Promise<User> {
     const password = (await User.findOne(user.id, { select: ["password"] })).password;
     if (!user || !this._jwt.verifyPassword(password, body.password))
       throw new ForbiddenException();
 
     user.password = this._jwt.encodePassword(body.new_password);
     await user.save();
+    return user.exclude(["password"]);
   }
 
   @Patch("/name")
   @UseGuards(UserGuard)
-  async updateName(@Body() body: UserNameDto, @GetUser() user: User) {
-    user.name = body.name;
-    await user.save();
+  async updateName(@Body() body: UserNameDto, @GetUser() user: User): Promise<User> {
+    if (await User.exists({ where: { name: body.name.toLowerCase() } }))
+      throw new BadRequestException();
+    user.name = body.name.toLowerCase();
+    return await user.save();
   }
 
   @Get("/me")
   @UseGuards(UserGuard)
-  async getInfo(@GetUser() user: User): Promise<UserRes> {
-    const userRes = await User.findOne({ relations: ["projects"], where: { id: user.id } });
-    this._logger.log(userRes.projects.length);
-    return new UserRes(userRes);
+  async getInfo(@GetUser({ joinProjects: true }) user: User): Promise<User> {
+    return user;
   }
 }
--- src/decorators/user.decorator.ts
+++ src/decorators/user.decorator.ts
@@ -4,11 +4,10 @@ import { AppLogger } from 'src/utils/app-logger.service';
 
 export const GetUser = createParamDecorator(async (data: GetUserOptions | null, ctx: ExecutionContext): Promise<User> => {
   const userId: string = ctx.switchToHttp().getRequest().headers.user ?? ctx.switchToWs().getClient().handshake.headers.user;
-  new AppLogger().log(userId);
   if (data?.joinProjects)
-    return await User.findOne({ relations: ["projects"], where: { id: userId }, select: ["id", 'name'] });
+    return await User.findOne(userId, { relations: ["projects"], select: ["id", 'name'] });
   else
-    return await User.findOne({ where: {id: userId}, select: ["id", "name"]});
+    return await User.findOne(userId, { select: ["id", "name"]});
 });
 
 export const GetUserId = createParamDecorator((data: void, ctx: ExecutionContext): string => {
