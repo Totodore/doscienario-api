f17a83d5321091cebbad76e3703cb925b6e39cf8
Totodore <prevottheodore@gmail.com>
2022-01-02 03:44:34 +0100

    adding logs to repo

---
+++ src/models/logs/logs.entity.ts
@@ -0,0 +1,25 @@
+import { User } from './../user/user.entity';
+import { BaseEntity, Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Logs extends BaseEntity {
+
+  @PrimaryGeneratedColumn()
+  public id: number;
+
+  @Column("text")
+  public message: string;
+
+  @Column("text")
+  public logs: string;
+
+  @Column("boolean", { default: false })
+  public solved: boolean;
+
+  @ManyToOne(() => User)
+  @JoinColumn()
+  public user: User;
+
+  @CreateDateColumn()
+  public createdDate: Date;
+}
