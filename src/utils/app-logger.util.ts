47f57974e26fde518a60ebbb34d28d00f69643a6
Totodore <prevottheodore@gmail.com>
2022-03-11 22:17:16 +0100

    Feature(Logging) Adding log origin

--- src/utils/app-logger.util.ts
+++ src/utils/app-logger.util.ts
@@ -1,21 +1,44 @@
-import { Logger, Module } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
-@Module({
-  providers: [AppLogger],
-  exports: [AppLogger],
-})
+@Injectable()
 export class AppLogger extends Logger {
 
-  log(...message: any[]) {
-    super.log(message.join(" "), "AppLogger");
+  public info(...message: any[]) {
+    super.log(message.join(" "), this.getCaller());
   }
-  warn(...message: any[]) {
-    super.warn(message.join(" "), "AppLogger");
+  public log(...message: any[]) {
+    super.log(message.join(" "), this.getCaller());
   }
-  debug(...message: any[]) {
-    super.debug(message.join(" "), "AppLogger")
+  public warn(...message: any[]) {
+    super.warn(message.join(" "), this.getCaller());
   }
-  verbose(...message: any[]) {
-    super.verbose(message.join(" "), "AppLogger");
+  public debug(...message: any[]) {
+    super.debug(message.join(" "), this.getCaller())
+  }
+  public verbose(...message: any[]) {
+    super.verbose(message.join(" "), this.getCaller());
+  }
+  public error(...message: (string | Error)[]) {
+    const error = message[message.length - 1];
+    if (error instanceof Error)
+      super.error(message.slice(0, -1), error.stack, this.getCaller());
+    else
+      super.error(message.join(" "), "", this.getCaller());
+  }
+
+  private getCaller(): string {
+    const error = new Error();
+    let stackLine = 3;
+    try {
+      throw error;
+    } catch (e) {
+      const stack = e.stack.split("\n");
+      const line: string = stack[stackLine];
+      const className: string = line.match(/([A-Z])\w+/g)?.[0] || "<anonymous>";
+      let methodName: string = line.match(/(?<=\.)(.+)(?= )/gi)?.[0] || "<anonymous>";
+      if (line.match(/new ([A-Z])\w+/g))
+        methodName = "constructor";
+      return `${className}::${methodName}`;
+    }
   }
 }
